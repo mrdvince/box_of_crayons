89ff309d06681406ca4d3ef14902c2deab96f4e6
Vince <vincivenv@gmail.com>
2021-10-28 06:47:31 +0300

    description on swagger

--- bocr/app/app/main.py
+++ bocr/app/app/main.py
@@ -9,7 +9,28 @@ from app.api.api_v1.api import api_router
 from app.core.config import settings
 
 app = FastAPI(
-    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+    title=settings.PROJECT_NAME,
+    description="""
+Given a plant leaf photo, 
+return a bounding box of the type of disease it has
+(limited to the 8 classes trained on). 
+
+Also get some answers to your question using OpenAI's GPT3 (Am on free credits so will be switching to the ada model from davinci)
+
+Model used is a YoloV5
+> Note: the dataset used was manually labelled and still needs to be improved for a better and more accurate model
+
+### Disease typess
+   'Apple Scab',
+    'Apple Cedar Rust',
+    'Apple Frogeye Spot',
+    'Maize Gray Leaf Spot',
+    'Maize Leaf Blight',
+    'Potato Blight',
+    'Tomato Bacteria Spot',
+    'Tomato Blight',
+    """,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
 )
 app.mount("/runs", StaticFiles(directory="runs", html=True), name="runs")
 
